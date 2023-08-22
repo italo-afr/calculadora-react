a5442191d045c4463b0414b412c3676a0ea194e2
unknown <italoafr1@gmail.com>
2023-08-22 20:44:24 -0300

    adicionando o arquivo

---
+++ src/Componentes/Calculadora/index.js
@@ -0,0 +1,27 @@
+import './calculadora.css'
+
+const Calculadora = () => {
+    return (<header className="botoes">
+        <button className='botao-set'>1</button>
+        <button className='botao-set'>2</button>
+        <button className='botao-set'>3</button>
+        <button className='botao-set'>4</button>
+        <button className='botao-set'>5</button>
+        <button className='botao-set'>6</button>
+        <button className='botao-set'>7</button>
+        <button className='botao-set'>8</button>
+        <button className='botao-set'>9</button>
+        <button className='botao-set'>0</button>
+        <button className='botao-set'>-/+</button>
+        <button className='botao-set'>=</button>
+        <button className='botao-set'>-</button>
+        <button className='botao-set'>+</button>
+        <button className='botao-set'>CE</button>
+        <button className='botao-set'>%</button>
+        <button className='botao-set'>/</button>
+        <button className='botao-set'>X</button>
+        <button className='botao-set'>,</button>
+    </header>)
+}
+
+export default Calculadora
